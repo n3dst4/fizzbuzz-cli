855034f8a4f958f631b5a63c4d48e25836fdc242
Neil de Carteret <ndecarteret@sportingindex.com>
2015-10-27 16:45:22 +0000

    tests

--- test/test-fizzbuzz.js
+++ test/test-fizzbuzz.js
@@ -1,21 +1,38 @@
+/*eslint no-console: 0*/
 /*global describe, it*/
 
 import chai from "chai";
-import fizzbuzz from "../fizzbuzz";
+import {exec} from 'child_process';
 
 const expect = chai.expect;
 
+const resultPromise = new Promise(function (resolve, reject) {
+  exec("node __build/fizzbuzz.js", function (error, stdout, stderr) {
+    if (error !== null) {
+      console.log(`exec error while running script: ${error}`);
+      process.exit(1);
+    }
+
+    const output = stdout.toString("utf-8");
+    const tokens = output.split("\n");
+    resolve(tokens);
+  });
+});
+
 describe("fizzbuzz", function () {
-  it("should print FizzBuzz for 0", function () {
-    expect(fizzbuzz(0, 1)).to.deep.equal(["FizzBuzz"]);
+  it("should do the first fifteen correctly", function () {
+    return resultPromise.then(result => {
+      expect(result.slice(1, 16)).to.deep.equal([
+        "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11",
+        "Fizz", "13", "14", "FizzBuzz"]);
+    });
   })
-  it("should print 1 for 1", function () {
-    expect(fizzbuzz(1, 2)).to.deep.equal(["1"]);
-  })
-  it("should do the first fifteen right", function () {
-    expect(fizzbuzz(1, 16)).to.deep.equal(["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]);
-  })
-  it("should do a high number", function () {
-    expect(fizzbuzz(3000000, 3000001)).to.deep.equal(["FizzBuzz"]);
+
+  it("should do the last fifteen correctly", function () {
+    return resultPromise.then(result => {
+      expect(result.slice(86, 101)).to.deep.equal([
+        "86", "Fizz", "88", "89", "FizzBuzz", "91", "92", "Fizz", "94", "Buzz",
+        "Fizz", "97", "98", "Fizz", "Buzz"]);
+    });
   })
 });
